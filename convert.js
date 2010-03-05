49067604d99ae5d8ca2ebcf232a412ca789711c0
strav <strav@strav-laptop.(none)>
2010-03-05 03:14:42 -0500

    Initial commit

---
+++ convert.js
@@ -0,0 +1,20 @@
+var style_element;
+
+function injectCss(cssToInject){
+    style_element = document.createElement("style");
+    style_element.innerText = cssToInject;
+    document.documentElement.insertBefore(style_element, null);
+}
+
+function removeCss(){
+    style_element.parentNode.removeChild(style_element);
+    history.go(0);
+}
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    if (request.action == "injectCss") {
+	injectCss(request.cssToInject);
+    } else if (request.action == "removeCss") {
+	removeCss();
+    }
+});
